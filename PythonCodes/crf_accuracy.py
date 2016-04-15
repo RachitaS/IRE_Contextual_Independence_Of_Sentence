076af4a09ebd5e99adb913d23efc5422ef6fa7df
Rachita Sowle <r.rachita.s@gmail.com>
2016-04-15 18:27:46 +0530

    Update crf_accuracy.py

--- PythonCodes/crf_accuracy.py
+++ PythonCodes/crf_accuracy.py
@@ -3,39 +3,26 @@ import sys
 
 resFile = str(sys.argv[1])
 # Open a file
-fo=open(resFile, "rw+")
+fo=open(resFile, "r")
+fout=open("Output_clasification", "wr")
 lines = fo.readlines()
 corr=0
 incorr=0
-c=0
-i=0
-space=' '
-fout=open("../Output/output"+str(sys.argv[2]), "wa")
-sent=""
 for line in lines:
 	lineArr=line.split('\t')
-	sent+=space+lineArr[0]
 	if (lineArr[0]=='\n'):
-		if(c>=i):
-#print "Classified:    \t {0}\n".format(sent)
-			fout.write("Classified:    \t {0}\n".format(sent))
-			corr+=1
-		else:
-#print "Misclassified: \t {0}\n".format(sent)
-			fout.write("Misclassified: \t {0}\n".format(sent))
-			incorr+=1
-		c=0
-		i=0
-		sent=""
+		fout.write('\n')
 		continue
-	if (lineArr[-1][0]==lineArr[-2]):
-		c+=1
+	
+	if (lineArr[-1][:-1]==lineArr[-2]):
+		fout.write(lineArr[0]+":\Classified")
+		corr+=1
 	else:
-		i+=1
+		incorr+=1
+		fout.write(lineArr[0]+":\Misclassified")
 # Close opend file
 fo.close()
 fout.close()
 
 acc = corr*100/(corr+incorr)
 print acc
-
