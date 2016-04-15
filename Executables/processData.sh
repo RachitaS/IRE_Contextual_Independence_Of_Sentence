caff917c94c1bb28cd0c845b53fc72091f4652b7
Rachita Sowle <r.rachita.s@gmail.com>
2016-04-15 18:33:22 +0530

    Update processData.sh

--- Executables/processData.sh
+++ Executables/processData.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-python ../PythonCodes/crf_foldData.py
+#Run this command 5 times for training and 5 times for testing 
+python ../PythonCodes/crf_foldData.py "training"
 
 echo 'Processing training and testing data for 5 fold completed'
    
